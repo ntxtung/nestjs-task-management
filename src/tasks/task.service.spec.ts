121324cbf73dbbe6d7cb2f5887f4dd87b278b4a3
Xuân Tùng <xuantung.1998@gmail.com>
2020-12-07 16:10:28 +0700

    add tests + before refactor snapshot

---
+++ src/tasks/task.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TaskRepository } from './task.repository';
+
+const mockTaskRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+describe('TaskService', () => {
+  let taskService;
+  let taskRepository;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        {
+          provide: TaskRepository,
+          useFactory: mockTaskRepository,
+        },
+      ],
+    }).compile();
+    taskService = await module.get<TasksService>(TasksService);
+    taskRepository = await module.get<TaskRepository>(TaskRepository);
+  });
+
+  it('should be defined', () => {
+    expect(true).toEqual(true);
+  });
+
+  describe('getTasks', () => {
+    it('should return all tasks from the repository', () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
